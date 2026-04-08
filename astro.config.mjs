fc1c973176498ed46712d1508017707421f35c8c
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 15:31:15 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,7 @@
 import { defineConfig } from 'astro/config';
-import DecapCMS from 'astro-decap-cms';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://37eight.app',
-  integrations: [
-    react(),
-    DecapCMS({
-      config: {
-        backend: {
-          name: 'git-gateway',
-          branch: 'latest',
-        },
-        media_folder: 'public/assets/blog',
-        public_folder: '/assets/blog',
-        collections: [
-          {
-            name: 'blog',
-            label: 'Статьи',
-            folder: 'src/pages/posts',
-            create: true,
-            fields: [
-              { label: 'Title', name: 'title', widget: 'string' },
-              { label: 'Body', name: 'body', widget: 'markdown' },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
+  outDir: 'dist',
+  publicDir: 'public'
 });

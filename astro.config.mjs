d0ee3f6ae58dc975dd6c685384ccff31c5ac5365
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 17:11:53 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import { sanityIntegration } from '@sanity/astro'; // Изменено название функции
+import sanity from '@sanity/astro'; // Используем импорт по умолчанию
 
 export default defineConfig({
   integrations: [
     react(),
     tailwind(),
-    sanityIntegration({
+    sanity({
       projectId: 'd4fi998k',
       dataset: 'production',
       studioPath: '/admin',

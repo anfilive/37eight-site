642e9feccecdef16ba60c4190bbe793bb89331cb
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 18:03:26 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@tailwindcss/vite';
-import sanity from '@sanity/astro'; // Импорт по умолчанию — самый надежный
+import sanity from '@sanity/astro'; 
+import vercel from '@astrojs/vercel'; // Добавляем адаптер Vercel
 
 export default defineConfig({
+  output: 'server', // Включаем серверный режим
+  adapter: vercel(), // Подключаем адаптер
   integrations: [
     react(),
     sitemap(),
-    // Используем просто sanity() вместо sanityIntegration()
     sanity({
       projectId: 'd4fi998k',
       dataset: 'production',

9afa545be717c99e3505191424c702cba37eda51
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 18:49:05 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@tailwindcss/vite';
 import sanity from '@sanity/astro'; 
-import vercel from '@astrojs/vercel'; // Добавляем адаптер Vercel
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server', // Включаем серверный режим
-  adapter: vercel(), // Подключаем адаптер
+  output: 'hybrid', // Гибридный режим — самый надежный
+  adapter: vercel({
+    webPolyfills: false // Явно отключаем то, на что ругалась ошибка
+  }),
   integrations: [
     react(),
     sitemap(),

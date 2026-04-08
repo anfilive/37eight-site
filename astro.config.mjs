5ac6daab8697111fd08af0adcc1a6316d8e001f5
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 21:43:59 +0300

    Update33 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
-// astro.config.mjs
-sanity({
-  projectId: 'd4fi998k',
-  dataset: 'production',
-  // studioPath: '/admin',  <-- УДАЛИТЕ ИЛИ ЗАКОММЕНТИРУЙТЕ ЭТУ СТРОКУ
-  useCdn: false,
-}),
- 
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@tailwindcss/vite';
+import sanity from '@sanity/astro'; 
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  trailingSlash: 'always',
+  integrations: [
+    react(),
+    sitemap(),
+    sanity({
+      projectId: 'd4fi998k',
+      dataset: 'production',
+      useCdn: false,
+    }),
+  ],
+  vite: {
+    plugins: [tailwind()],
+  },
+});

fc7ca6d97808a9e696d97377c020836648407e64
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 17:23:56 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@tailwindcss/vite';
-import sanity from '@sanity/astro'; // Импортируем без фигурных скобок
+import { sanityIntegration } from '@sanity/astro';
+import netlify from '@astrojs/netlify'; // Добавили адаптер
 
 export default defineConfig({
+  output: 'server', // Это переключает сайт в режим "умного" сервера
+  adapter: netlify(), // Это связывает Astro и Netlify
   integrations: [
     react(),
-    sanity({ // Используем просто как sanity()
+    sanityIntegration({
       projectId: 'd4fi998k',
       dataset: 'production',
-      studioPath: '/admin',
+      studioPath: '/admin', // Путь к твоей админке
       useCdn: false,
     }),
   ],

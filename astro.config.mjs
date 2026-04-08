eba68f116fc29cd515fe72c1027b6cba8d87a994
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 17:29:23 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@tailwindcss/vite';
-import sanity from '@sanity/astro'; // Берем импорт по умолчанию
-import netlify from '@astrojs/netlify';
+import { sanityIntegration } from '@sanity/astro';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  // Удалили output: 'server' и adapter: netlify()
+  // Теперь сайт будет статическим и максимально надежным
   integrations: [
     react(),
     sitemap(),
-    sanity({ // Используем просто как sanity
+    sanityIntegration({
       projectId: 'd4fi998k',
       dataset: 'production',
       studioPath: '/admin',

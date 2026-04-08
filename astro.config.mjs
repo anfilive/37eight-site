7bd6f9c4081144b02cc9e007a8204abc0db5b242
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 17:19:56 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@tailwindcss/vite';
-import { sanityIntegration } from '@sanity/astro';
+import sanity from '@sanity/astro'; // Импортируем без фигурных скобок
 
 export default defineConfig({
   integrations: [
     react(),
-    sitemap(),
-    sanityIntegration({
+    sanity({ // Используем просто как sanity()
       projectId: 'd4fi998k',
       dataset: 'production',
       studioPath: '/admin',

0c2b48d90af08d297c3f7488f2710002dd1b44a0
anfilive <151508774+anfilive@users.noreply.github.com>
2026-04-08 14:37:25 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,55 +1,35 @@
 import { defineConfig } from 'astro/config';
 import DecapCMS from 'astro-decap-cms';
+import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://37eight.app',
   integrations: [
+    react(), // Добавь это обязательно!
     DecapCMS({
       config: {
-        // Use Netlify’s “Git Gateway” authentication and target our default branch
         backend: {
           name: 'git-gateway',
           branch: 'latest',
         },
-        // Configure where our media assets are stored & served from
         media_folder: 'public/assets/blog',
         public_folder: '/assets/blog',
-        // Configure the content collections
         collections: [
           {
             name: 'posts',
             label: 'Blog Posts',
-            label_singular: 'Blog Post',
             folder: 'src/pages/posts',
             create: true,
-            delete: true,
             fields: [
               { name: 'title', widget: 'string', label: 'Post Title' },
-              {
-                name: 'publishDate',
-                widget: 'datetime',
-                format: 'DD MMM YYYY',
-                date_format: 'DD MMM YYYY',
-                time_format: false,
-                label: 'Publish Date',
-              },
-              { name: 'author', widget: 'string', label: 'Author Name', required: false },
-              { name: 'authorURL', widget: 'string', label: 'Author URL', required: false },
+              { name: 'publishDate', widget: 'datetime', label: 'Publish Date' },
               { name: 'description', widget: 'string', label: 'Description', required: false },
               { name: 'body', widget: 'markdown', label: 'Post Body' },
-              {
-                name: 'layout',
-                widget: 'select',
-                default: '../../layouts/BlogPost.astro',
-                options: [
-                  { label: 'Blog Post', value: '../../layouts/BlogPost.astro' },
-                ],
-              },
+              { name: 'layout', widget: 'hidden', default: '../../layouts/BlogPost.astro' },
             ],
           },
         ],
       },
-      previewStyles: ['/src/styles/blog.css'],
     }),
   ],
 });
